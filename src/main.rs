5e61ccd08a9a43ccca70367864f3aab4c1f4fafb
may h <mehrabhoque@gmail.com>
2020-11-27 15:34:50 -0500

    Add a note about another, faster possible approach
    This one benefits from natural parallelism, but introduces a security
    concern (vector?).

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,19 @@ mod config;
 
 #[get("/")]
 fn index(gallery: State<config::Gallery>) -> Template {
+    // TODO: This is not as performant as encoding an API endpoint that returns the image bytes.
+    // Doing the API endpoint method lets us get natural parallelism, as the browser will fire off
+    // a request for each image individually, whereas, currently, we are serially writing the bytes
+    // of the images into the HTML file.
+    //
+    // That said, allowing endpoints gives access to the disk in some capacity, which is a big
+    // security concern. Likely, we can get it done securely by only returning file URLs that the
+    // gallery is tracking.
+    //
+    // That said, I am risk averse enough (and dumb enough) to not trust myself to write that code
+    // correctly, so I'll tolerate the slower page load. Perhaps when I use this with enough
+    // images, it'll take so damn long that I'll bite the bullet. Right now, I'm not there yet.
     Template::render("index", gallery.inner())
-    // format!("Hello, world!: {:?}", gallery)
 }
 
 fn main() -> Result<()> {
