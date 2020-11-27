1cd8ebc5209d22471e25fc66ad2844e28cac99e1
may h <mehrabhoque@gmail.com>
2020-11-27 13:30:48 -0500

    Add the Gallery type

--- src/config.rs
+++ src/config.rs
@@ -1,14 +1,9 @@
 use std::env;
-use std::fs::File;
+use std::fs::{read_dir, DirEntry, File, ReadDir};
 use std::path::PathBuf;
 
 use anyhow::{Error, Result};
 use serde::Deserialize;
-
-#[derive(Deserialize, Debug)]
-pub struct GalleryConfig {
-    dirs: Vec<PathBuf>,
-}
 
 pub fn load_config(config_path: &str) -> Result<GalleryConfig> {
     let config_file = File::open(config_path)?;
@@ -22,5 +17,35 @@ pub fn parse_config_path_from_args_or_die() -> Result<String> {
         Ok(first_arg)
     } else {
         Err(Error::msg("no configuration file argument specified"))
+    }
+}
+
+#[derive(Deserialize, Debug)]
+pub struct GalleryConfig {
+    dirs: Vec<PathBuf>,
+}
+
+pub struct Gallery<'a> {
+    cfg: &'a GalleryConfig,
+    dir_entries: Vec<PathBuf>,
+}
+
+impl<'a> Gallery<'a> {
+    fn new(cfg: &'a GalleryConfig) -> Result<Gallery> {
+        let mut dir_iters: Vec<ReadDir> = Vec::new();
+        for dir in &cfg.dirs {
+            let dir_iter = read_dir(dir.as_path())?;
+            dir_iters.push(dir_iter);
+        }
+
+        let results: Vec<DirEntry> = dir_iters
+            .iter_mut()
+            .flatten()
+            .collect::<Result<Vec<DirEntry>, std::io::Error>>()?;
+
+        Ok(Gallery {
+            cfg,
+            dir_entries: results.iter().map(|x| x.path()).collect(),
+        })
     }
 }
